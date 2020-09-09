a12917bdfeb9ec1cf12e0989626a4d74a729a6fe
eduardomagno <eduardo.magno.u@gmail.com>
2020-09-09 12:31:09 -0300

    feat:logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg'
 import './App.css';
 
 const App = () => {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={"https://cristine.balsini.gitlab.io/pytroski/img/Photo%20de%20Perfil%20Roma%20Aeterna.jpg"} className="App-logo" alt="logo" />
+				<img src={logo} className="App-logo" alt="logo" />
 				<p>Written by Augusto Pietroski</p>
 			</header>
 		</div>
