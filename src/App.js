e481baddb1a7087be9dc44f1c521e580772032bc
Pietroski <AugustoPTSantos@gmail.com>
2020-09-09 19:36:40 -0300

    feat: login screen not functional

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import logo from './assets/images/logo.svg'
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import Login from './pages/login';
 
 const App = () => {
 	return (
 		<Switch>
 			<Route exact path="/">
-				<div className="App">
-					<header className="App-header">
-						<img src={logo} />
-					</header>
-				</div>
+				<Login />
 			</Route>
 		</Switch>
 	);
