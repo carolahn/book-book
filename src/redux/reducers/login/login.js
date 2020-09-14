ecc855c30a1047ec7b7da571fdd2aab8922d079c
Augusto Pietroski <augustoptsantos@gmail.com>
2020-09-14 17:42:54 +0000

    Update login.js

--- src/redux/reducers/login/login.js
+++ src/redux/reducers/login/login.js
@@ -1,44 +1,29 @@
-import LOGIN_ACTIONS from "../../actions/login/login-action-types";
+import LOGIN_ACTIONS from '../../actions/login/login-action-types';
 
 const defaultState = {
-  error: "",
-  error_message: "",
-  username: "",
-  token: localStorage.getItem("book-book-token")
-    ? localStorage.getItem("book-book-token")
-    : "",
-  login_status: false,
-};
+    error: '',
+    error_message: '',
+    username: '',
+    token: localStorage.getItem('book-book-token') ? localStorage.getItem('book-book-token') : "",
+    login_status: localStorage.getItem('book-book-token') ? true : false,
+}
 
 const reducer = (state = defaultState, action) => {
-  switch (action.type) {
-    case LOGIN_ACTIONS.LOGIN_SUCCESSFUL:
-      localStorage.setItem("book-book-token", action.token);
-      return {
-        ...state,
-        username: action.username,
-        token: action.token,
-        id: action.id,
-        login_status: true,
-      };
+    switch (action.type) {
+        case LOGIN_ACTIONS.LOGIN_SUCCESSFUL: 
+            localStorage.setItem('book-book-token', action.token);
+            return { ...state, username: action.username, token: action.token, login_status: true};
 
-    case LOGIN_ACTIONS.LOGIN_UNSUCCESSFUL:
-      return {
-        ...state,
-        error: action.error,
-        error_message: "invalid credencials",
-        login_status: false,
-        token: "",
-      };
+        case LOGIN_ACTIONS.LOGIN_UNSUCCESSFUL:
+            return { ...state, error: action.error, error_message: "invalid credencials", login_status: false, token: ''};
 
-    case LOGIN_ACTIONS.LOGOUT:
-      localStorage.getItem("book-book-token") &&
-        localStorage.removeItem("book-book-token");
-      return { ...defaultState };
+        case LOGIN_ACTIONS.LOGOUT:
+            localStorage.getItem('book-book-token') && localStorage.removeItem('book-book-token');
+            return { ...defaultState, token: '', login_status: false};
 
-    default:
-      return state;
-  }
-};
+        default:
+            return state;
+    }    
+}
 
 export default reducer;
