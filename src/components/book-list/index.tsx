6eea739351a0fd196ce74dfcb7761d541813b3f5
Cassiano Bitencourt <tricecautopo@gmail.com>
2020-09-17 17:30:49 -0300

    feat(?): book-list typescript

---
+++ src/components/book-list/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { Container, WrapBook } from "./styles.js";
+import Book from "../book";
+import AsideDescription from "../aside-description";
+
+interface CurrBookType {
+  description: string;
+  review: string;
+}
+
+interface DisplayProps {
+  showBooks: CurrBookType[];
+  type: string;
+  page: number;
+  size: { width: number };
+}
+
+const BookList: React.FC<DisplayProps> = ({ showBooks, type, page, size }) => {
+  return (
+    <Container>
+      <div>
+        {showBooks &&
+          showBooks
+            .slice((page - 1) * 10, (page - 1) * 10 + 10)
+            .map((currBook, key: number) => (
+              <WrapBook key={key}>
+                {size.width < 745 ? (
+                  <Book bookData={currBook} type={type.concat("-mobile")} />
+                ) : (
+                  <>
+                    <Book bookData={currBook} type={type.concat("-desktop")} />
+                    <AsideDescription
+                      type={type.concat("-desktop")}
+                      description={currBook.description}
+                      review={currBook.review}
+                      bookData={currBook}
+                    />
+                  </>
+                )}
+              </WrapBook>
+            ))}
+      </div>
+    </Container>
+  );
+};
+
+export default BookList;
